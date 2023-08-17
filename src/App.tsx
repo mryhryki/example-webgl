ce0b2a6adff6962d984dd8ee2653662774dcfcee
mryhryki <mryhryki@gmail.com>
2023-08-17 15:59:08 +0900

    Update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,31 +11,33 @@ function App() {
   useEffect(() => {
     if (canvasRef == null) return;
 
-    const camera = new THREE.PerspectiveCamera(90, width / height, 0.01, 10);
-    camera.position.z = 1;
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvasRef });
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(window.devicePixelRatio);
 
     const scene = new THREE.Scene();
 
-    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    // const geometry = new THREE.PlaneGeometry(width, height);
+    const camera = new THREE.OrthographicCamera();
+    camera.position.set(0, 0, 1);
+
+    // const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    const geometry = new THREE.PlaneGeometry(1, 1);
     // const material = new THREE.MeshNormalMaterial();
     const material = new THREE.MeshBasicMaterial({
+      side: THREE.DoubleSide,
       map: new THREE.TextureLoader().load("/images/854x1280.jpg"),
     });
-
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvasRef });
-    renderer.setSize(width, height);
-
-    function animation(time: number) {
-      mesh.rotation.x = time / 4000;
-      mesh.rotation.y = time / 4000;
-      renderer.render(scene, camera);
-    }
-
-    renderer.setAnimationLoop(animation);
+    renderer.render(scene, camera);
+    // function animation(time: number) {
+    //   mesh.rotation.x = time / 4000;
+    //   mesh.rotation.y = time / 4000;
+    //   renderer.render(scene, camera);
+    // }
+    //
+    // renderer.setAnimationLoop(animation);
   }, [
     canvasRef,
   ]);
