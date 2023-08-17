160266b7c0a334e3c1a6ec5bce037287d3431162
mryhryki <mryhryki@gmail.com>
2023-08-17 15:26:13 +0900

    Show texture

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,40 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import * as THREE from "three";
 
+const width = 1280;
+const height = 800;
+
 function App() {
   const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     if (canvasRef == null) return;
 
-    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    const camera = new THREE.PerspectiveCamera(90, width / height, 0.01, 10);
     camera.position.z = 1;
 
     const scene = new THREE.Scene();
 
     const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    const material = new THREE.MeshNormalMaterial();
+    // const geometry = new THREE.PlaneGeometry(width, height);
+    // const material = new THREE.MeshNormalMaterial();
+    const material = new THREE.MeshBasicMaterial({
+      map: new THREE.TextureLoader().load("/images/854x1280.jpg"),
+    });
 
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvasRef });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(width, height);
 
     function animation(time: number) {
-      mesh.rotation.x = time / 2000;
-      mesh.rotation.y = time / 1000;
+      mesh.rotation.x = time / 4000;
+      mesh.rotation.y = time / 4000;
       renderer.render(scene, camera);
     }
 
     renderer.setAnimationLoop(animation);
-    document.body.appendChild(renderer.domElement);
   }, [
     canvasRef,
   ]);
@@ -37,7 +43,7 @@ function App() {
   return (
     <>
       <h1>Example WebGL</h1>
-      {/*<canvas ref={setCanvasRef} style={{ width: 1280, height: 800 }}/>*/}
+      <canvas ref={setCanvasRef} style={{ width, height }}/>
     </>
 
   );
